9ec0065f49531e7e23bd99a75016c648cc0122fa
MATUTINO <MATUTINO@DESKTOP-NCQJGHM>
2024-06-10 09:10:47 -0500

    scv008

--- src/com/distribuida/entities/Factura_detalle.java
+++ src/com/distribuida/entities/Factura_detalle.java
@@ -1,13 +1,6 @@
 package com.distribuida.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-import org.springframework.stereotype.Component;
 
 //@Component
 //@Entity
